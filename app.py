804a68140c61de344a83211b86fb1679fec99995
rietmjoe <rietmjoe@students.zhaw.ch>
2024-04-11 15:05:01 +0200

    Frontend finish für hüt

--- app.py
+++ app.py
@@ -16,6 +16,10 @@ import streamlit as st
 # Setze die Seitenkonfiguration
 st.set_page_config(page_title="Entdecke lokale Aktivitäten", layout="wide")
 
+
+# Pagegestaltung Header
+st.image("berg2.jpg", use_column_width=True)
+
 # Stelle das Logo dar
 col1, col2 = st.columns([1, 3])
 with col1:
@@ -23,48 +27,24 @@ with col1:
 with col2:
     st.write("")
 
-
-# Initialisiere den Bildindex im Session State, falls noch nicht vorhanden
-if 'bild_index' not in st.session_state:
-    st.session_state.bild_index = 0
-    
-# Bildliste
-bilder = ["luzern.jpg", "berg.jpg", "berg2.jpg"]
-
-# Container für das Bild und die Navigation
-col1, col2, col3 = st.columns([1, 3, 1])
-
-# Knopf für das vorherige Bild
-with col1:
-    if st.button('◀️'):
-        # Gehe zum vorherigen Bild, bleibe bei 0, wenn bereits am Anfang
-        st.session_state.bild_index = max(st.session_state.bild_index - 1, 0)
-
-# Zeige das aktuelle Bild
-with col2:
-    st.image(bilder[st.session_state.bild_index], use_column_width=True)
-
-# Knopf für das nächste Bild
-with col3:
-    if st.button('▶️'):
-        # Gehe zum nächsten Bild, bleibe am Ende, wenn bereits am letzten Bild
-        st.session_state.bild_index = min(st.session_state.bild_index + 1, len(bilder) - 1)
-
-
-
-# Willkommenstitel und -untertitel
 st.title("Entdecke, was du in deiner Nähe unternehmen kannst!")
 st.subheader("Gib deinen Standort und das Wetter ein, und wir schlagen dir vor, was du unternehmen kannst.")
 
-# Formular für Benutzereingaben
+
+
+
+# Benutzereingaben
 with st.form("user_input"):
-    # Texteingabe für die Postleitzahl mit vornotiertem Wert
+
+    # Sie hend no irgend e Kategorie wölle. Luegemer mol was machbar isch.
+    category = st.selectbox("Was suchst du?", ["Kultur", "Sport", "Touristenattraktion"])
+
+    # PLZ Eingabe mit St. Gallen als Default 
     location = st.number_input("Gib deine Postleitzahl ein", value=9000, min_value=1000, max_value=9999, step=1)
 
-    # Dropdown-Menü für Wetteroptionen
-    weather = st.selectbox("Wähle das Wetter aus", ["Regnerisch", "Sonnig", "Bewölkt"])
+    # Dropdown für Wetter
+    weather = st.selectbox("Wähle das Wetter aus", ["Sonnig", "Regnerisch", "Bewölkt"])
     
-    # Submit-Button für das Formular
     submitted = st.form_submit_button("Vorschläge anzeigen")
     if submitted:
         st.write(f"Für die Postleitzahl {location} bei {weather} Wetter, empfehlen wir folgendes:")
@@ -76,5 +56,3 @@ with st.form("user_input"):
             st.write("Geh besser nicht an den See")
         else:
             st.write("Ein Spaziergang im bewölkten Wetter kann erfrischend sein!")
-
-
